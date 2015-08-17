6a472c5eeb6b2edf47ab24374d5bc29d1949fffb
Bishwa Hang Rai <bishwahang.kirat@gmail.com>
2015-08-16 21:55:00 -0700

    xclip, ctags and chrome

--- README.md
+++ README.md
@@ -5,9 +5,7 @@ Install minimum required app, and set up configs.
 
 ```
 sudo apt-get -y --force-yes update && sudo apt-get -y install \
-  build-essential \
-  git vim-gnome tmux
-
+  build-essential git
 
 git clone https://github.com/bishwahang/dotfiles.git ~/.dotfiles
 cd ~/.dotfiles && ./bare_minimum.sh && ./config.sh
--- bare_minimum.sh
+++ bare_minimum.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # set solarized
+echo "setting solarized"
 cd
 wget --no-check-certificate https://raw.github.com/seebi/dircolors-solarized/master/dircolors.ansi-dark
 mv dircolors.ansi-dark .dircolors
@@ -9,9 +10,19 @@ git clone https://github.com/sigurdga/gnome-terminal-colors-solarized.git
 cd gnome-terminal-colors-solarized
 ./set_dark.sh
 cd
+# rbenv and pyenv
 echo "installing rbenv"
 git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 echo "installing pyenv and pyenv-virtualenv"
 git clone https://github.com/yyuu/pyenv.git ~/.pyenv
 git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
+
+# install other packages
+echo "installing packages"
+sudo apt-get -y install \
+  libxss1 libappindicator1 libindicator7 \
+  vim-gnome tmux exuberant-ctags xclip
+echo "installing chrome"
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome*.deb
 echo "Finished bare minimum"
